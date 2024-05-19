05f21df6182d3d35f328a6022a703757e7a3fca2
Abdullah_Abdullaev <86735829+abdullahproff@users.noreply.github.com>
2024-05-20 00:36:19 +0300

    Add files via upload

---
+++ src/common/struct.h
@@ -0,0 +1,26 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct flag_type {
+  int b;
+  int n;
+  int s;
+  int v;
+  int T;
+  int E;
+} flag_type;
+
+typedef struct grep_flag {
+  int e;
+  int i;
+  int v;
+  int c;
+  int l;
+  int n;
+  int h;
+  int s;
+  int f;
+  int o;
+} grep_flag;
+
+#endif
